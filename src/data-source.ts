5b4929c3c3c33a38960b1706e7a6a00e200c7531
HeyitzSagar <this.sagar.gupta@gmail.com>
2025-06-30 12:23:47 +0530

    FIXED: password in env

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,17 @@
 // src/data-source.ts
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+import { dot } from 'node:test/reporters';
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
   host: 'localhost',
   port: 3306,
   username: 'root',
-  password: 'Sagar@321',
+  password: process.env.DB_PASS || '', // Use environment variable for password
   database: 'docusign_db',
   synchronize: true,
   logging: false,
